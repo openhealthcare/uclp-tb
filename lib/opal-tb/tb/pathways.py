c9577a53b82c2d19daa35514fe0b518bcb2b7d9d
fredkingham <fredkingham@gmail.com>
2016-06-10 11:56:24 +0100

    move investigations into opal

--- lib/opal-tb/tb/pathways.py
+++ lib/opal-tb/tb/pathways.py
@@ -17,22 +17,22 @@ class TBScreening(RedirectsToPatientMixin, Pathway):
     display_name = "TB Screening"
     slug = "tb_screening"
     steps = (
-    #     uclptb_models.Demographics,
-    #     # inline first name and surname hide middle name
-    #
-    #     uclptb_models.ContactDetails,
-    #
-    #     uclptb_models.ReferralRoute,
-    #
-    #     # combine the 2 into one custom step
-    #     tb_models.EnvironmentalTBRiskFactors,
-    #     # this needs display logic work
-    #
-    #     # tb_models.MedicalTBRiskFactors,
-    #     # inline check boxes
-    #
-    #     MultSaveStep(model=uclptb_models.SymptomComplex),
-    #     MultSaveStep(model=uclptb_models.PastMedicalHistory),
+        uclptb_models.Demographics,
+        # inline first name and surname hide middle name
+
+        uclptb_models.ContactDetails,
+
+        uclptb_models.ReferralRoute,
+
+        # combine the 2 into one custom step
+        tb_models.EnvironmentalTBRiskFactors,
+        # this needs display logic work
+
+        # tb_models.MedicalTBRiskFactors,
+        # inline check boxes
+
+        MultSaveStep(model=uclptb_models.SymptomComplex),
+        MultSaveStep(model=uclptb_models.PastMedicalHistory),
 
         # combine the 2 into one custom step
         MultSaveStep(
