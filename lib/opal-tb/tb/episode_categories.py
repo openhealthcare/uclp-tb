3adaa9b38ca905494033f152fbd990ef47549b6e
fredkingham <fredkingham@gmail.com>
2016-06-27 17:51:21 +0100

    add in contact tracing

--- lib/opal-tb/tb/episode_categories.py
+++ lib/opal-tb/tb/episode_categories.py
@@ -1,5 +1,11 @@
 from opal.core import episodes
 
+
+class TBEpisodeStages(object):
+    CONTACT_TRACING = "Contact tracing"
+
+
 class TBEpisode(episodes.EpisodeCategory):
     display_name = "TB"
     detail_template = "detail/tb.html"
+    stages = TBEpisodeStages
