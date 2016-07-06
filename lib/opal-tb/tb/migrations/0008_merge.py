deef6199a3e6083662ce5a8b51b0e6054dc0ff90
David Miller <david@deadpansincerity.com>
2016-07-06 17:41:40 +0100

    Add merge migration

---
+++ lib/opal-tb/tb/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tb', '0007_auto_20160706_1555'),
+        ('tb', '0007_auto_20160706_1323'),
+    ]
+
+    operations = [
+    ]
