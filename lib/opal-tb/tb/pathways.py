888d1f6e7ad3bd9fb93f2636438572d3483ee007
fredkingham <fredkingham@gmail.com>
2016-07-15 18:20:22 +0100

    rearranged contact tracing and moved it into a sinlge page with the multistep logic

--- lib/opal-tb/tb/pathways.py
+++ lib/opal-tb/tb/pathways.py
@@ -21,10 +21,9 @@ class TBContactTracing(RedirectsToPatientMixin, Pathway):
     display_name = "Contact Tracing"
     slug = "contact_tracing"
     steps = (
-        tb_models.EnvironmentalRiskAssessment,
-        MultSaveStep(
+        Step(
             model=tb_models.ContactTracing,
-            controller_class="ContactTracingFormCtrl"
+            template_url="/templates/contact_tracing.html"
         ),
     )
 
