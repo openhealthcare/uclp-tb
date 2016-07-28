509b130c814b5b1c76d5bf8b42006ffd70a002f5
David Miller <david@deadpansincerity.com>
2016-07-28 15:18:45 +0100

    Add patient pathway should set a start date

--- lib/opal-tb/tb/tests/test_pathways.py
+++ lib/opal-tb/tb/tests/test_pathways.py
@@ -1,14 +1,26 @@
+"""
+Unittests for the tb.pathways module
+"""
+import datetime
+
 from opal.core.test import OpalTestCase
-from tb.pathways import TBTreatment, TreatmentOutcome
 from opal.models import Episode
 from uclptb.models import Treatment
 
+from tb import pathways
+
+class TestTBAddPatientTestCase(OpalTestCase):
+    def test_save_sets_episode_start_date(self):
+        pathway = pathways.TBAddPatient(patient_id=None, episode_id=None)
+        patient = pathway.save({'demographics': [{'hospital_number': '1234'}]}, self.user)
+        episode = patient.episode_set.first()
+        self.assertEqual(datetime.date.today(), episode.date_of_admission)
 
 
 class TestTBTreatment(OpalTestCase):
     def test_save(self):
         patient, episode = self.new_patient_and_episode_please()
-        pathway = TBTreatment(patient_id=patient.id, episode_id=episode.id)
+        pathway = pathways.TBTreatment(patient_id=patient.id, episode_id=episode.id)
         example_data = dict(stage=['Active TB'], treatment=[])
         pathway.save(example_data, self.user)
         reloaded_episode = Episode.objects.get()
@@ -18,7 +30,7 @@ class TestTBTreatment(OpalTestCase):
 class TestTreatmentOutcome(OpalTestCase):
     def test_save(self):
         patient, episode = self.new_patient_and_episode_please()
-        pathway = TreatmentOutcome(
+        pathway = pathways.TreatmentOutcome(
             patient_id=patient.id, episode_id=episode.id
         )
         pathway.save({}, self.user)
