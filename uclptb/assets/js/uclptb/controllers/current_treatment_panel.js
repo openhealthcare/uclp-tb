602153f05843a5e15aa8e628bd2c811d458b43d2
fredkingham <fredkingham@gmail.com>
2016-08-24 11:20:06 +0100

    add use strict

--- uclptb/assets/js/uclptb/controllers/current_treatment_panel.js
+++ uclptb/assets/js/uclptb/controllers/current_treatment_panel.js
@@ -1,3 +1,4 @@
 angular.module('opal.controllers').controller('CurrentTreatmentPanelCtrl', function(treatmentUtils){
+  "use strict";
   this.getActiveTreatments = treatmentUtils.getActiveTreatments;
 });
